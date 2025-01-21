a2b785fa7036e94d94f673f382f484ff7abe769e
Tirsasaki <tirsasaki@gmail.com>
2025-01-21 13:10:19 +0700

    perbaikan kecil

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: 'Neta Lynx',
-  subtitle: 'Linux Specialist',
+  subtitle: 'Linux Specialist skilled in troubleshooting and system optimization',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -29,10 +29,10 @@ export const siteConfig: SiteConfig = {
     depth: 2                // Maximum heading depth to show in the table, from 1 to 3
   },
   favicon: [    // Leave this array empty to use the default favicon
-    {
-      src: '/public/favicon/favicon.png',    // Path of the favicon, relative to the /public directory
+      {
+         src: '/public/favicon/favicon.png',    // Path of the favicon, relative to the /public directory
     //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+         sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
     }
   ]
 }
