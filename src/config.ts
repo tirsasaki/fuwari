998c6f7acd6a7042ad199434e611b793e596200b
Tirsasaki <tirsasaki@gmail.com>
2025-01-21 19:37:35 +0700

    The Linux Specialist's Guide to System Performance

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: 'Neta Lynx',
-  subtitle: 'Linux Specialist skilled in troubleshooting and system optimization',
+  subtitle: 'The Linux Specialist Guide to System Performance',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
